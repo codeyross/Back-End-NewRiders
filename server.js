52f4ce42a203a292bd1ee4533ccc2c0beeaef2ad
BradleyHazel <bradleyhazel@gmail.com>
2022-06-02 23:00:56 -0400

    try

--- server.js
+++ server.js
@@ -3,41 +3,43 @@
 require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
+const methodOverride = require("method-override");
 
 const app = express();
 
 const mongoose = require("mongoose");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
+const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
 const nodemailer = require("nodemailer");
 const session = require("express-session")
 var cookieParser = require("cookie-parser")
 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({credentials: true, origin: true}));
-app.use(function(req, res, next) {
-    res.header({
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Access-Control-Allow-Headers": "Content-Type, *"
-    });
-    next();
-});
-app.use(cookieParser())
 
-app.use(session({
-  secret: '2C44-4D44-WppQ38S',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: process.env.DEV_USER_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {httpOnly:false},
+
+  })
+);
+
+
+app.use(cookieParser(process.env.DEV_USER_SECRET))
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("port", process.env.PORT || 8001);
 
 User = require("./models/user-model");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -59,9 +61,25 @@ passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GOOGLE_CLIENT_ID = process.env.CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.CLIENT_SEC;
 
 var userProfile;
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.CLIENT_REDIRECT,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      userProfile = profile;
+      return done(null, userProfile);
+    }
+  )
+);
 /*
 const loginlimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
@@ -107,7 +125,6 @@ app.post(
 );
 
 app.get("/checkAuthentication", (req, res) => {
-  console.log(req)
   const authenticated =  req.isAuthenticated()
 
   res.status(200).json({
