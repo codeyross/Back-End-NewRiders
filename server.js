bd737dd4791002c1d24ab64927f65a312139da22
BradleyHazel <bradleyhazel@gmail.com>
2022-06-03 21:49:08 -0400

    Deploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(
 
 
 app.use(cookieParser(process.env.DEV_USER_SECRET))
-app.use(cors({credentials: true, origin: "https://629ab602ac0b810ea4a5a2b4--curious-pika-6c6244.netlify.app"}));
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
 
 app.use(express.static("public"));
 app.use(express.json());
